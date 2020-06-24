432d3436994b7ab60625c954fcf52e50e81c8b66
saifsms91 <saifsms91@rediffmail.com>
2020-06-25 00:22:00 +0800

    Code Refactor

--- src/test/java/screens/androidpageobjects/LandingPageAndroid.java
+++ src/test/java/screens/androidpageobjects/LandingPageAndroid.java
@@ -10,58 +10,46 @@ import utils.CommonUtils;
 
 public class LandingPageAndroid extends ScreenBase {
 
-	String USERNAME = "";
-	String PASSWORD = "";
+    String USERNAME = "";
+    String PASSWORD = "";
 
-	public LandingPageAndroid() {
-		try {
-			Properties properties = CommonUtils.read_properties();
-			USERNAME = properties.getProperty("email");
-			PASSWORD = properties.getProperty("password");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    public LandingPageAndroid() {
+        try {
+            Properties properties = CommonUtils.read_properties();
+            USERNAME = properties.getProperty("email");
+            PASSWORD = properties.getProperty("password");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	By sign_in_button = By.id("in.amazon.mShop.android.shopping:id/sign_in_button");
-	By ap_email_login = By.xpath("//*[@resource-id='ap_email_login']");
-	By continueBtn = By.xpath("//*[@resource-id='continue']");
+    By sign_in_button = By.id("in.amazon.mShop.android.shopping:id/sign_in_button");
+    By ap_email_login = By.xpath("//*[@resource-id='ap_email_login']");
+    By continueBtn = By.xpath("//*[@resource-id='continue']");
+    By ap_password = By.xpath("//*[@resource-id='ap_password']");
+    By signInSubmit = By.xpath("//*[@resource-id='signInSubmit']");
 
+    public void tapSignInOnLandingPage() {
+        System.out.println("tapSignInOnLandingPage: " + driver); // printed to see if the tapSignInOnLandingPage is
+        // working
+        driver.findElement(sign_in_button).click();
+    }
 
-	By ap_password = By.xpath("//*[@resource-id='ap_password']");
+    public void enterNumberOrEmail() {
+        driver.findElement(ap_email_login).sendKeys(USERNAME);
+    }
 
+    public void tapContinueBtn() {
+        driver.findElement(continueBtn).click();
+    }
 
-	By signInSubmit = By.xpath("//*[@resource-id='signInSubmit']");
+    public void enterPassword() {
+        driver.findElement(ap_password).sendKeys(PASSWORD);
+    }
 
-	public void tapSignInOnLandingPage() {
-		System.out.println("tapSignInOnLandingPage: " + driver); // printed to see if the tapSignInOnLandingPage is
-																	// working
-		driver.findElement(sign_in_button).click();
-	}
-
-	public void enterNumberOrEmail() {
-
-		driver.findElement(ap_email_login).sendKeys(USERNAME);
-
-	}
-
-	public void tapContinueBtn() {
-
-		driver.findElement(continueBtn).click();
-
-	}
-
-	public void enterPassword() {
-
-		driver.findElement(ap_password).sendKeys(PASSWORD);
-
-	}
-
-	public void tapSignInSubmit() {
-
-		driver.findElement(signInSubmit).click();
-
-	}
+    public void tapSignInSubmit() {
+        driver.findElement(signInSubmit).click();
+    }
 
 }

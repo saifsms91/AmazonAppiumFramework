264be56ab4514f03406c0ff510cacf102eab1196
saifsms91 <saifsms91@rediffmail.com>
2020-06-29 00:56:41 +0800

    WIP

--- src/test/java/screens/androidpageobjects/AmazonHomePageAndroid.java
+++ src/test/java/screens/androidpageobjects/AmazonHomePageAndroid.java
@@ -10,25 +10,36 @@ public class AmazonHomePageAndroid extends ScreenBase {
 	String Settings;
 
 	By action_bar_burger_icon = By.id("in.amazon.mShop.android.shopping:id/action_bar_burger_icon");
-	By setting_bar_burger = By.xpath(
-			"//android.widget.TextView[@content-desc=\"Settings button. Double tap for links to change country, sign out, and more.\"]");
+
+	By setting_bar_burger = By.xpath("//*[@resource-id='in.amazon.mShop.android.shopping:id/drawer_icon']");
+
 	By sign_out_setting_burger = By.xpath(
 			"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[5]/android.widget.LinearLayout");
 	By sign_out_popup = By.id("android:id/button2");
 	By usrname_display_burger = By.id("in.amazon.mShop.android.shopping:id/gno_greeting_text_view");
-	By Shop_by_Category = By.xpath(
-			"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.TextView");
-	By TV_Appliances_Electronics = By.xpath(
-			"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.ViewAnimator/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.webkit.WebView/android.webkit.WebView/android.view.View[1]/android.view.View/android.view.View[11]/android.view.View[1]/android.view.View[4]");
-	By Headphones = By.xpath(
-			"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.ViewAnimator/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.webkit.WebView/android.webkit.WebView/android.view.View[1]/android.view.View/android.view.View[11]/android.view.View[20]");
 
-	By deals_idp_first_headphone = By.id("idp-first-image");
 
-	By Sennheiser_CX_180_Street_II_In = By.xpath(
-			"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.ViewAnimator/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.LinearLayout[2]/android.widget.FrameLayout/android.widget.ViewSwitcher/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup");
+	By Shop_by_Category = By.xpath("//*[contains(@text,'Shop by Category')]");
 
-	By add_to_cart_button = By.id("add-to-cart-button");
+
+
+	By TV_Appliances_Electronics = By.xpath("//*[contains(@text,'TV, Appliances, Electronics')]");
+
+
+
+	By Headphones = By.xpath("//*[contains(@text,'Headphones')]");
+
+
+
+	By deals_idp_first_headphone = By.xpath("//*[contains(@text,'Bose SoundSport, Wireless Earbuds, (Sweatproof Bluetooth Headphones for Running and Sports), Aqua')]");
+
+
+	By Bose = By.xpath("//*[contains(@text,'Bose SoundSport, Wireless Earbuds, (Sweatproof Bluetooth Headphones for Running and Sports), Aqua')]");
+
+
+
+	By add_to_cart_button = By.xpath("//*[contains(@text,'Add to Cart')]");
+
 
 	public void tapAction_bar_burger_icon() {
 		driver.findElement(action_bar_burger_icon).click();
@@ -95,7 +106,7 @@ public class AmazonHomePageAndroid extends ScreenBase {
 
 	{
 
-		driver.findElement(Sennheiser_CX_180_Street_II_In).click();
+		driver.findElement(Bose).click();
 
 	}
 

58261f775132330d41f7c65a9e93e80f50dc37ca
saifsms91 <saifsms91@rediffmail.com>
2020-06-29 13:53:40 +0800

    WIP

--- src/test/java/screens/androidpageobjects/AmazonHomePageAndroid.java
+++ src/test/java/screens/androidpageobjects/AmazonHomePageAndroid.java
@@ -7,115 +7,100 @@ import org.openqa.selenium.interactions.touch.TouchActions;
 import base.ScreenBase;
 
 public class AmazonHomePageAndroid extends ScreenBase {
-	String Settings;
+    String Settings;
 
-	By action_bar_burger_icon = By.id("in.amazon.mShop.android.shopping:id/action_bar_burger_icon");
+    By action_bar_burger_icon = By.id("in.amazon.mShop.android.shopping:id/action_bar_burger_icon");
 
-	By setting_bar_burger = By.xpath("//*[@resource-id='in.amazon.mShop.android.shopping:id/drawer_icon']");
+    By setting_bar_burger = By.xpath("//*[@resource-id='in.amazon.mShop.android.shopping:id/drawer_icon']");
 
-	By sign_out_setting_burger = By.xpath(
-			"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[5]/android.widget.LinearLayout");
-	By sign_out_popup = By.id("android:id/button2");
-	By usrname_display_burger = By.id("in.amazon.mShop.android.shopping:id/gno_greeting_text_view");
+    By sign_out_setting_burger = By.xpath(
+            "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[5]/android.widget.LinearLayout");
 
+    By sign_out_popup = By.id("android:id/button2");
 
-	By Shop_by_Category = By.xpath("//*[contains(@text,'Shop by Category')]");
-
+    By usrname_display_burger = By.id("in.amazon.mShop.android.shopping:id/gno_greeting_text_view");
 
 
-	By TV_Appliances_Electronics = By.xpath("//*[contains(@text,'TV, Appliances, Electronics')]");
+    By Shop_by_Category = By.xpath("//*[contains(@text,'Shop by Category')]");
 
 
-
-	By Headphones = By.xpath("//*[contains(@text,'Headphones')]");
-
+    By TV_Appliances_Electronics = By.xpath("//*[contains(@text,'TV, Appliances, Electronics')]");
 
 
-	By deals_idp_first_headphone = By.xpath("//*[contains(@text,'Bose SoundSport, Wireless Earbuds, (Sweatproof Bluetooth Headphones for Running and Sports), Aqua')]");
+    By Headphones = By.xpath("//*[contains(@text,'Headphones')]");
 
 
-	By Bose = By.xpath("//*[contains(@text,'Bose SoundSport, Wireless Earbuds, (Sweatproof Bluetooth Headphones for Running and Sports), Aqua')]");
+    By deals_idp_first_headphone = By.xpath("//*[contains(@text,'Bose SoundSport, Wireless Earbuds, (Sweatproof Bluetooth Headphones for Running and Sports), Aqua')]");
 
 
+    By Bose = By.xpath("//*[contains(@text,'Bose SoundSport, Wireless Earbuds, (Sweatproof Bluetooth Headphones for Running and Sports), Aqua')]");
 
-	By add_to_cart_button = By.xpath("//*[contains(@text,'Add to Cart')]");
+
+    By add_to_cart_button = By.xpath("//*[contains(@text,'Add to Cart')]");
 
 
-	public void tapAction_bar_burger_icon() {
-		driver.findElement(action_bar_burger_icon).click();
-	}
+    public void tapAction_bar_burger_icon() {
+        driver.findElement(action_bar_burger_icon).click();
+    }
 
-	public void tapSetting_bar_burger() {
+    public void tapSetting_bar_burger() {
 
-		driver.findElement(setting_bar_burger).click();
+        driver.findElement(setting_bar_burger).click();
 
-	}
+    }
 
-	public void tapSign_out_setting_burger() {
-		driver.findElement(sign_out_setting_burger).click();
+    public void tapSign_out_setting_burger() {
 
-	}
+        driver.findElement(sign_out_setting_burger).click();
 
-	public void tapSign_out_popup() {
-		driver.findElement(sign_out_popup).click();
-	}
+    }
 
-	public String getusrname_display_burger()
+    public void tapSign_out_popup() {
+        driver.findElement(sign_out_popup).click();
+    }
 
-	{
+    public String getusrname_display_burger() {
 
-		return driver.findElement(usrname_display_burger).getText();
+        return driver.findElement(usrname_display_burger).getText();
 
-	}
+    }
 
-	public void tap_Shop_by_Category()
+    public void tap_Shop_by_Category() {
 
-	{
+        driver.findElement(Shop_by_Category).click();
 
-		driver.findElement(Shop_by_Category).click();
+        System.out.println("Tap on Shop_by_Category ");
 
-		System.out.println("Tap on Shop_by_Category ");
+    }
 
-	}
+    public void tapTV_Appliances_Electronics() {
 
-	public void tapTV_Appliances_Electronics()
+        driver.findElement(TV_Appliances_Electronics).click();
 
-	{
+    }
 
-		driver.findElement(TV_Appliances_Electronics).click();
+    public void tapHeadphones() {
 
-	}
+        driver.findElement(Headphones).click();
 
-	public void tapHeadphones()
+    }
 
-	{
+    public void dealsHeadphonesCarousel() {
 
-		driver.findElement(Headphones).click();
+        driver.findElement(deals_idp_first_headphone).click();
 
-	}
+    }
 
-	public void dealsHeadphonesCarousel()
+    public void addFirstHeadphonesListing() {
 
-	{
+        driver.findElement(Bose).click();
 
-		driver.findElement(deals_idp_first_headphone).click();
+    }
 
-	}
+    public void addFirstHeadphonesToCart() {
 
-	public void addFirstHeadphonesListing()
+        driver.findElement(add_to_cart_button).click();
 
-	{
-
-		driver.findElement(Bose).click();
-
-	}
-
-	public void addFirstHeadphonesToCart()
-
-	{
-
-		driver.findElement(add_to_cart_button).click();
-
-	}
+    }
 
 }

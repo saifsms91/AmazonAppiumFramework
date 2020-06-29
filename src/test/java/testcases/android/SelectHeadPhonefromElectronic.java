39fc4e0d84701886c96e91d10aae4608e1d5ca2e
saifsms91 <saifsms91@rediffmail.com>
2020-06-29 13:54:33 +0800

    WIP

--- src/test/java/testcases/android/SelectHeadPhonefromElectronic.java
+++ src/test/java/testcases/android/SelectHeadPhonefromElectronic.java
@@ -1,53 +1,50 @@
 package testcases.android;
 
-		import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertEquals;
 
-		import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 
-		import base.TestBase;
-		import io.qameta.allure.Description;
-		import screens.androidpageobjects.AmazonHomePageAndroid;
-		import screens.androidpageobjects.LandingPageAndroid;
+import base.TestBase;
+import io.qameta.allure.Description;
+import screens.androidpageobjects.AmazonHomePageAndroid;
+import screens.androidpageobjects.LandingPageAndroid;
 
-public class SelectHeadPhonefromElectronic extends TestBase{
+public class SelectHeadPhonefromElectronic extends TestBase {
 
-	public LandingPageAndroid LandingPage = new LandingPageAndroid();
-	public AmazonHomePageAndroid AmazonHomePage = new AmazonHomePageAndroid();
+    public LandingPageAndroid LandingPage = new LandingPageAndroid();
+    public AmazonHomePageAndroid AmazonHomePage = new AmazonHomePageAndroid();
 
-	@Test(priority = 3, enabled = true)
-	@Description ("Add 1st headphone in cart")
-	public void headPhoneSection() throws Exception {
+    @Test(priority = 2, enabled = true)
+    @Description("Add 1st headphone in cart")
+    public void headPhoneSection() throws Exception {
 
-		System.out.println("Select Headphone: " + driver);
+        System.out.println("Select Headphone: " + driver);
 //		AmazonHomePage.tapAction_bar_burger_icon();
 //		System.out.println("tapAction_bar_burger_icon");
 
 
-		AmazonHomePage.tap_Shop_by_Category();
+        AmazonHomePage.tap_Shop_by_Category();
 
 
-		AmazonHomePage.tapTV_Appliances_Electronics();
+        AmazonHomePage.tapTV_Appliances_Electronics();
+        System.out.println("Select tapTV_Appliances_Electronics: ");
 
-		System.out.println("Select tapTV_Appliances_Electronics: ");
+        AmazonHomePage.tapHeadphones();
+        System.out.println("Select tapHeadphones: ");
 
-		AmazonHomePage.tapHeadphones();
-
-		System.out.println("Select tapHeadphones: ");
-
-		AmazonHomePage.dealsHeadphonesCarousel();
-		System.out.println("Select dealsHeadphonesCarousel: ");
+        AmazonHomePage.dealsHeadphonesCarousel();
+        System.out.println("Select dealsHeadphonesCarousel: ");
 
 
-		AmazonHomePage.addFirstHeadphonesListing();
-		System.out.println("Select addFirstHeadphonesListing: ");
+        AmazonHomePage.addFirstHeadphonesListing();
+        System.out.println("Select addFirstHeadphonesListing: ");
 
 
-		AmazonHomePage.addFirstHeadphonesToCart();
+        AmazonHomePage.addFirstHeadphonesToCart();
+        System.out.println("Select addFirstHeadphonesToCart: ");
 
-		System.out.println("Select addFirstHeadphonesToCart: ");
 
-
-	}
+    }
 
 
 }

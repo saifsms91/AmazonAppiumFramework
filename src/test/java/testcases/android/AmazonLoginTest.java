8b870d6a6249f816d8f7b9d5bc7a730fc06f0618
saifsms91 <saifsms91@rediffmail.com>
2020-06-29 13:54:16 +0800

    WIP

--- src/test/java/testcases/android/AmazonLoginTest.java
+++ src/test/java/testcases/android/AmazonLoginTest.java
@@ -11,45 +11,44 @@ import screens.androidpageobjects.AmazonHomePageAndroid;
 import screens.androidpageobjects.LandingPageAndroid;
 
 public class AmazonLoginTest extends TestBase {
-	public LandingPageAndroid LandingPage = new LandingPageAndroid();
-	public AmazonHomePageAndroid AmazonHomePage = new AmazonHomePageAndroid();
+    public LandingPageAndroid LandingPage = new LandingPageAndroid();
+    public AmazonHomePageAndroid AmazonHomePage = new AmazonHomePageAndroid();
 
-	@Test(priority = 1, enabled = true)
-	@Description ("Verify Valid login for Amazon")
-	public void verifyValidLogin() throws Exception {
+    @Test(priority = 1, enabled = true)
+    @Description("Verify Valid login for Amazon")
+    public void verifyValidLogin() throws Exception {
 
-	System.out.println("verifyValidLogin: " + driver);
-		LandingPage.tapSignInOnLandingPage();
-		LandingPage.enterNumberOrEmail();
-		LandingPage.tapContinueBtn();
-		LandingPage.enterPassword();
-		LandingPage.tapSignInSubmit();
-		AmazonHomePage.tapAction_bar_burger_icon();
-		String VALIDATEUSRNAME = AmazonHomePage.getusrname_display_burger();
+        System.out.println("verifyValidLogin: " + driver);
+        LandingPage.tapSignInOnLandingPage();
+        LandingPage.enterNumberOrEmail();
+        LandingPage.tapContinueBtn();
+        LandingPage.enterPassword();
+        LandingPage.tapSignInSubmit();
+        AmazonHomePage.tapAction_bar_burger_icon();
+        String VALIDATEUSRNAME = AmazonHomePage.getusrname_display_burger();
 
-		System.out.println("VALIDATEUSRNAME" + VALIDATEUSRNAME);
+        System.out.println("VALIDATEUSRNAME" + VALIDATEUSRNAME);
+        assertEquals(VALIDATEUSRNAME, "Hello, sudhanshu");
 
-		assertEquals(VALIDATEUSRNAME, "Hello, Saif");
-
-	}
+    }
 
 
-	@Test(priority = 2, enabled = true)
-	@Description ("Logout from Amazon")
+    @Test(priority = 3, enabled = true)
+    @Description("Logout from Amazon")
 
-	public void verifyLogout() throws Exception {
+    public void verifyLogout() throws Exception {
 
-		AmazonHomePage.tapAction_bar_burger_icon();
-		System.out.println("tapAction_bar_burger_icon");
+        AmazonHomePage.tapAction_bar_burger_icon();
+        System.out.println("tapAction_bar_burger_icon");
 
-		AmazonHomePage.tapSetting_bar_burger();
-		System.out.println("tapSetting_bar_burger");
+        AmazonHomePage.tapSetting_bar_burger();
+        System.out.println("tapSetting_bar_burger");
 
-		AmazonHomePage.tapSign_out_setting_burger();
-		System.out.println("tapSign_out_setting_burger");
+        AmazonHomePage.tapSign_out_setting_burger();
+        System.out.println("tapSign_out_setting_burger");
 
-		AmazonHomePage.tapSign_out_popup();
-		System.out.println("tapSign_out_popup");
+        AmazonHomePage.tapSign_out_popup();
+        System.out.println("tapSign_out_popup");
 
-	}
+    }
 }

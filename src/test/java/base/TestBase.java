834923e47c581de2b16ab23ef8886ee29b56c2b2
saifsms91 <saifsms91@rediffmail.com>
2020-09-10 17:28:35 +0800

    Code refactor

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -1,37 +1,36 @@
 package base;
-
 import java.net.MalformedURLException;
 
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import io.appium.java_client.AppiumDriver;
 import screens.androidpageobjects.LandingPageAndroid;
-
 import utils.CommonUtils;
+import utils.AppConfigTags;
+import utils.Constants;
 
 public class TestBase {
 
-	public static AppiumDriver driver;
-	public LandingPageAndroid LandingPage;
+    public static AppiumDriver driver;
+    public LandingPageAndroid LandingPage;
 
-	private static final String ANDROID_URI = "/Users/saif.siddiqui/Downloads/Appium/src/test/resources/app/AmazonIndia.apk";
 
-	@BeforeSuite
-	public void setUp() throws MalformedURLException {
+    @BeforeSuite
+    public void setUp() throws MalformedURLException {
 
-		pageObjectConfig();
-		System.out.println("Setup TestCase");
+        pageObjectConfig();
+        System.out.println("Setup TestCase");
 
-		CommonUtils utils = new CommonUtils();
+        CommonUtils utils = new CommonUtils();
 
-		utils.setup("Android", "Motorola", ANDROID_URI);
-		driver = utils.driver;
+        utils.setup(AppConfigTags.ANDROID, AppConfigTags.MOTOROLA, Constants.ANDROID_URI);
+        driver = utils.driver;
 
-	}
+    }
 
-	private void pageObjectConfig() {
+    private void pageObjectConfig() {
 
-	}
+    }
 
 	@AfterSuite
 	public void tearDown() {
